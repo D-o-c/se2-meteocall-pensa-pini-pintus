37c47fc4eaddf878942a6525f0017549c0bc4a36
doc <mpini91@gmail.com>
2014-12-19 10:57:11 +0100

    first

---
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/security/boundary/PublicArea.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.business.security.boundary;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author doc
+ */
+@Stateless
+public class PublicArea {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
